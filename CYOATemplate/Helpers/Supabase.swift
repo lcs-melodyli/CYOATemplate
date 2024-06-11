02532e18cf29030848c38850936e92156a0c766a
Melody Li <zli@lcs.on.ca>
2024-06-11 15:48:26 -0400

    Initial Commit

---
+++ CYOATemplate/Helpers/Supabase.swift
@@ -0,0 +1,18 @@
+//
+//  Supabase.swift
+//  CYOATemplate
+//
+//  Created by Russell Gordon on 2024-06-02.
+//
+
+import Foundation
+import Supabase
+
+// Mr. Gordon's exemplar
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://xqjxbpfiihexfmggmhdi.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxanhicGZpaWhleGZtZ2dtaGRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTczMzkwNDcsImV4cCI6MjAzMjkxNTA0N30.l9Q_6DzrImCorkF25mSEokB8QxzsYD9T-cJFXjEJ3U8"
+)
+
+
+
